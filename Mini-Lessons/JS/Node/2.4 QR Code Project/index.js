1763d5a6596d0ecf63da77b21a7282592f088e0a
Illia Kovalenko <illia_nova@Illias-MBP.la.net.ua>
2023-08-31 17:11:05 +0300

    add QR code project

---
+++ Mini-Lessons/JS/Node/2.4 QR Code Project/index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer.prompt([
+    {
+        name: 'URL',
+        message: 'Type in your url here:',
+    }
+]).then((answers) =>
+{
+    const url = answers.URL;
+
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream("url_qr_code.png"));
+
+    fs.writeFile("url.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    })
+});
